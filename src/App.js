7a31ca4477caabecde4e11f5c40f1633ebebe123
Timo Leinonen <splinecl@gmail.com>
2020-08-24 13:32:05 +0200

    Stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  componentDidMount() {
+    fetch('http://localhost:8080/greeting')
+        .then(res => res.json())
+        .then((data) => {
+          console.log('data ' + JSON.stringify(data));
+          this.setState({ rowData: data })
+        })
+        .catch(console.log)
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: [{
+        headerName: "Game", field: "game", sortable: true, filter: true
+      }, {
+        headerName: "Player", field: "player", sortable: true, filter: true
+      }, {
+        headerName: "Score", field: "score", sortable: true, filter: true
+      }]
+    }
+  }
+
+  render() {
+    return (
+        <div
+            className="ag-theme-alpine"
+            style={{
+              height: '250px',
+              width: '600px' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <AgGridReact
+              columnDefs={this.state.columnDefs}
+              rowData={this.state.rowData}>
+          </AgGridReact>
+        </div>
+    );
+  }
 }
 
 export default App;
