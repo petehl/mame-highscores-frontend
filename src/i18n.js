fa5a83180e721a403a693fb45fe0f2924c6a78a6
Timo Leinonen <splinecl@gmail.com>
2020-08-26 14:38:42 +0200

    Translations for rom names, danish!

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,19 @@
-import i18n from "i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
-import XHR from "i18next-xhr-backend";
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translationEng from "./locales/en/translation.json";
+import translationDa from "./locales/da/translation.json";
 
 i18n
-    .use(XHR)
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
     .init({
         debug: true,
-        lng: "en",
+
         fallbackLng: "en", // use en if detected lng is not available
 
         keySeparator: false, // we do not use keys in form messages.welcome
@@ -20,6 +25,9 @@ i18n
         resources: {
             en: {
                 translations: translationEng
+            },
+            da: {
+                translations: translationDa
             }
         },
         // have a common namespace used around the full app
